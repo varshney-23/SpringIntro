d6d01b4b050192d356cac8d92a1e5cbe83980729
varshney-23 <ayushivarshney40@gmail.com>
2025-03-01 22:53:47 +0530

    path variable

--- src/main/java/com/springdemo/springintro/controller/HelloController.java
+++ src/main/java/com/springdemo/springintro/controller/HelloController.java
@@ -1,9 +1,6 @@
 package com.springdemo.springintro.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -17,6 +14,11 @@ public class HelloController {
 
     @GetMapping(value = "/query")
     public String query(@RequestParam String name){
+        return "Hello " + name + " from bridgelabz";
+    }
+
+    @GetMapping("/hello/param/{name}")
+    public String helloParam(@PathVariable String name){
         return "Hello " + name + " from bridgelabz";
     }
 }
