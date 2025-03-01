2ae8f99b3704f7ccd54db1463cd69814a1d5e935
varshney-23 <ayushivarshney40@gmail.com>
2025-03-01 22:46:04 +0530

    query parameter

--- src/main/java/com/springdemo/springintro/controller/HelloController.java
+++ src/main/java/com/springdemo/springintro/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.springdemo.springintro.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,5 +13,10 @@ public class HelloController {
     @GetMapping(value = "/home")
     public String sayHello(){
         return "Hello From BridgeLabz";
+    }
+
+    @GetMapping(value = "/query")
+    public String query(@RequestParam String name){
+        return "Hello " + name + " from bridgelabz";
     }
 }
