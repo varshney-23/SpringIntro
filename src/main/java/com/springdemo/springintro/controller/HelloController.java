3bd1d3449b3ba01fe8c3faa900f75f7736cd3a8f
varshney-23 <ayushivarshney40@gmail.com>
2025-03-01 23:07:42 +0530

    put request

--- src/main/java/com/springdemo/springintro/controller/HelloController.java
+++ src/main/java/com/springdemo/springintro/controller/HelloController.java
@@ -13,20 +13,28 @@ public class HelloController {
         return "Hello From BridgeLabz";
     }
 
+    //http://localhost:8080/query?name= ayushi
     @GetMapping(value = "/query")
     public String query(@RequestParam String name){
         return "Hello " + name + " from bridgelabz";
     }
 
+    //http://localhost:8080/hello/param/ayushi
     @GetMapping("/hello/param/{name}")
     public String helloParam(@PathVariable String name){
         return "Hello " + name + " from bridgelabz";
     }
 
+    //http://localhost:8080/post
     @PostMapping("/post")
     public String sayHello(@RequestBody User user){
         return "Hello"+user.getFirstName() +" "+ user.getLastName()+" !";
     }
 
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable  String firstName,
+                           @RequestParam(value = "lastName") String lastName){
+        return "Hello "+firstName+" "+lastName+" !";
+    }
 
 }
