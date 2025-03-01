05788bfaad1dea5fde6879fdc9374cf0901fc0c0
varshney-23 <ayushivarshney40@gmail.com>
2025-03-01 22:40:25 +0530

    hellocontroller

--- src/main/java/com/springdemo/springintro/controller/HelloController.java
+++ src/main/java/com/springdemo/springintro/controller/HelloController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.springdemo.springintro.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
